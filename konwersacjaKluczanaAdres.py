7290c82a72b5c700da49ac1e401f5a12fdc18d1a
georgezyvg <georgeputta355@gmail.com>
2026-06-01 12:44:54 +0100

    Update konwersacjaKluczanaAdres.py

--- konwersacjaKluczanaAdres.py
+++ konwersacjaKluczanaAdres.py
@@ -12,7 +12,7 @@ z1 = int("0cf7190cc6c1f95b987e0e284e4eba44552f89662272b850b059a8dc0d8905a8   ", 
 
 # ✅ Dane z drugiej transakcji
 r2 = int("ceda0e7cfe7e6da20b3e1b08877e722eceba96574f50b78c8b03618e4c6ce18c", 16)
-s2 = int("5571c0b9af7fe3d6d04173e52b119eca11cc94eb882fa1228add9e206eeb5543", 16)
+s2 = int("034a6987bc4e6cfac6a8a5ed767ccbbf47cfb15323b3ebb44f3e72ee6148e255", 16)
 z2 = int("b91d628805f11c602df82fd34e792e604963efd6e08c7efac188d176ae561319", 16)
 
 # ✅ Stała wartość krzywej secp256k1 (order n)
